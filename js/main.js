d33cf72bce05fd90df84c34187defc1e44b12da4
Noel Jane Bourg <noeljanebourg@gmail.com>
2017-12-11 16:30:10 -0800

    start button that generates obstacles

--- js/main.js
+++ js/main.js
@@ -1,38 +1,69 @@
 /*
- 
-Notes from help with Phillippe
-
--Can use JS to calculate the width of the screen
-
--creating a lot of obstacles so you can't use variables
-
--Use a constructor function to create obstacles
-
--Build obstacles with constructor function, pass a speed so that the obstacle travels toward the player
-
--Can test it in the console
-
-Today- create the obstacle
-
-Tomorrow- create the player 
-
--------------------
 
 Steps to get this work done: 
 
-1. Link JS and CSS files
-2. Create an obstacle using a variable
+1. Link JS and CSS files x
+2. Create an obstacle using a variable x
 3. Create a constructor function 
 4. Think about how the obstacle is moving across the screen
-5. Create divs that measure where the obstacle needs to be in order to get the skiier to jump over it. 
+5. Create divs that measure where the obstacle needs to be in order to get the skier to jump over it. 
+6. Move the skier and obstacle lower down
+7. Is it possible to set it up vertically? 
 
 
 */
 
-//Variables
+// Variables
+var $body = $('body')
 var $obstacle = $('.obstacle')
 $obstacle.css('left', window.innerWidth)
 
-setInterval(function(){
-    $obstacle.css('left', "-=5px")
-},5000)
+//Variables to create multiple speeds and obstacles
+var obstacleNames = ["tree","rock","ogre"]
+var colors = ["green","black","grey"]
+var speeds = [1000, 500, 100]
+
+//Variable for start button
+var $start = $('#start')
+
+//Functions
+//Function to create more obstacles
+$start.on("click", function (){
+    console.log("You clicked start! Gnarly, bro!")
+    var $newObstacle = $('<div>')
+    $newObstacle.css({
+        "position": "absolute",
+        "top":"window.innerHeight",
+        "color": "forestgreen",
+        "border": "2px solid black",
+        "width": "100px",
+        "height": "100px",     
+    })
+    $body.append($newObstacle)
+    setInterval(function(){
+        $newObstacle.css('top','-=5px')
+    }, 500)
+    
+})
+
+// // Constructor Function ? 
+
+// function obstacle(name,color){
+//     this.name = name
+//     this.print = function(){
+//         console.log("I'm a " + name)
+//     }
+//     this.move = function (){
+//         setInterval(function(){
+//             $obstacle.css('left', '-=5px')
+
+//         }, 500)
+//     }
+// }
+
+
+//Function
+
+
+//New obstacles 
+// var tree = new obstacle('tree')
